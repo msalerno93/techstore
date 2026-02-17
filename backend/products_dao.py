d0fffa859db70e5ac4fd282d9ab78e6cdeb38c8e
Michael <msaler7@wgu.edu>
2026-02-17 10:19:30 -0500

    add/delete products functional

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -20,6 +20,23 @@ def get_all_products(connection):
         })
     return response
 
+def add_product(connection, product):
+    cursor = connection.cursor()
+    query = ("INSERT INTO tech_app.products (name, unit_id, price_per_unit) "
+            "VALUES (%s, %s, %s)")
+    data = (product['name'], product['unit_id'], product['price_per_unit'])
+    cursor.execute(query, data)
+    connection.commit()
+    return cursor.lastrowid
+
+def delete_product(connection, product_id):
+    cursor = connection.cursor()
+    query = ("DELETE FROM tech_app.products WHERE product_id = %s")
+    data = (product_id,)
+    cursor.execute(query, data)
+    connection.commit()
+    return cursor.rowcount
+
 if __name__ == "__main__":
     connection = get_sql_connection()
-    print(get_all_products(connection))
+    print(delete_product(connection, 3))
