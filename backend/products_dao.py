27412be3e74528cd6df42fb9fdb8d4b43db5bd2f
Michael <msaler7@wgu.edu>
2026-02-17 10:02:03 -0500

    pull all products from SQL DB, moved db connection

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -1,14 +1,25 @@
-import mysql.connector
+from sql_conn import get_sql_connection
 
-cnx = mysql.connector.connect(user='root',
-                                password= 'ApexLegends5293!',
-                                host= '127.0.0.1', # or the host IP address
-                                database= 'tech_app')
+from sql_conn import get_sql_connection
+def get_all_products(connection):
 
-cursor = cnx.cursor()
-query = 'SELECT * FROM tech_app.products'
-cursor.execute(query)
+    cursor = connection.cursor()
+    query = ("SELECT products.product_id, products.name, products.unit_id, products.price_per_unit, unit_measure.unit_name "
+                "FROM tech_app.products inner join tech_app.unit_measure on products.unit_id=unit_measure.unit_id;")
+    cursor.execute(query)
 
-for (product_id, name, unit_id, price_per_unit) in cursor:
-    print(product_id, name, unit_id, price_per_unit)
-cnx.close()
+    response = []
+
+    for (product_id, name, unit_id, price_per_unit, unit_name) in cursor:
+        response.append({
+            "product_id": product_id,
+            "name": name,
+            "unit_id": unit_id,
+            "price_per_unit": price_per_unit,
+            "unit_name": unit_name
+        })
+    return response
+
+if __name__ == "__main__":
+    connection = get_sql_connection()
+    print(get_all_products(connection))
